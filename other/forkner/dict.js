6712b789ea8d9fe78ce930fbcd17ea165d7c97eb
Patrick Rollings <patrickrollings@gmail.com>
2019-08-13 20:47:32 +0000

    Update other/forkner/dict.js

--- other/forkner/dict.js
+++ other/forkner/dict.js
@@ -46,8 +46,8 @@
             img_box.src = result.img;
         } else {
             img_box.src = "";
-            search_box.classList.add("flash");
-            setTimeout(() => search_box.classList.remove("flash"), 1000);
+            search_box.classList.add('flash');
+            setTimeout(() => search_box.classList.remove('flash'), 1000);
         }
     }
 

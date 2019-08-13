c4d65abff7d97c590e3543cdc52af0e31d2e843d
Patrick Rollings <patrickrollings@gmail.com>
2019-08-13 15:50:51 +0000

    Update other/forkner/dict.js

--- other/forkner/dict.js
+++ other/forkner/dict.js
@@ -1,7 +1,16 @@
 (() => {
+    let imgs = new Array;
+
+    function image_loaded(img) {
+        imgs.push(img);
+        console.log(img);
+    }
+
     function manifest_loaded (manifest) {
         manifest.split('\n').forEach((v, idx) => {
-            console.log("file" + v);
+            fetch("./assets/dict/" + v)
+                .then(res => res.blob())
+                .then(res => image_loaded(res));
         });
     } 
 

b7a48dab2225bd1555b8db32378a4dc332677d1b
Patrick Rollings <patrickrollings@gmail.com>
2019-08-13 14:55:44 +0000

    Update other/forkner/dict.js

--- other/forkner/dict.js
+++ other/forkner/dict.js
@@ -1,2 +1,8 @@
 (() => {
+    let file = new XMLHttpRequest();
+    file.open("GET", "./assets/manifest.txt");
+    file.onreadystatechange = () => {
+        let allText = file.responseText;
+        console.log(allText);
+    };
 })();

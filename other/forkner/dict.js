c147e0b15aa148e6375ce735f22eaeeab5b81d95
Patrick Rollings <patrickrollings@gmail.com>
2019-08-21 12:55:11 +0000

    Update other/forkner/dict.js

--- other/forkner/dict.js
+++ other/forkner/dict.js
@@ -1,10 +1,7 @@
 (() => {
     let search_box = document.getElementById("search_box");
-    let img_box = document.getElementById("img_box");
-    let word_caption = document.getElementById("word_caption");
-    let imgs = {};
 
-    function create_word_node(word) {
+    function create_word_element(word) {
         let cce = (parent, child_type) => parent.appendChild(document.createElement(child_type));
         let mb = document.getElementById("main_box");
         let word_node = cce(mb, 'div');
@@ -19,54 +16,6 @@
         wn_img_container.className = "word_img_container";
         wn_img.className = "word_img";
         wn_img.setAttribute('src', 'assets/' + word + '.svg');
-    }
-
-    function image_loaded(file_name, img) {
-        // lower case of word and remove symbols and abv marker
-        // store original word (with capital and/or apostrophe) in `display`
-        if (file_name == "") {
-            return;
-        }
-        let word = file_name.split('.')[0];
-        let is_abv = word.endsWith("_abv");
-        if (is_abv) {
-            word = word.split('_')[0];
-        }
-        let display = word;
-        let proper_noun = word[0] != word[0].toLowerCase();
-        if (proper_noun) {
-            word = word.toLowerCase();
-        }
-        img = URL.createObjectURL(img);
-        imgs[word] = {
-            img: img,
-            abv: is_abv,
-            proper_noun: proper_noun,
-            display: display,
-        };
-    }
-
-    function manifest_loaded(manifest) {
-        let promises = [];
-        manifest.split('\n').forEach((v, idx) => {
-            let promise = fetch("./assets/dict/" + v)
-                .then(res => res.blob())
-                .then(res => image_loaded(v, res));
-            promises.push(promise);
-        });
-        return Promise.all(promises);
-    }
-
-    function search(text) {
-        text = text.toLowerCase();
-        let result = imgs[text];
-        if (result) {
-            word_caption.innerHTML = result.display;
-            img_box.src = result.img;
-        } else {
-            word_caption.innerHTML = "";
-            img_box.src = "";
-        }
     }
 
     window.onload = () => {
